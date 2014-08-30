aa6eb4e7e3daba84f109d29afb04b146418ed7e1
Ryan Brewster <brewster1134+tower@gmail.com>
2014-08-30 00:25:01 -0700

    detect_type

--- lib/sourcerer.rb
+++ lib/sourcerer.rb
@@ -1,12 +1,14 @@
 require 'open-uri'
 
 class Sourcerer
-  attr_reader :dest_dir, :source, :tmp_dir
+  attr_reader :dest_dir, :source, :tmp_dir, :type
 
   def initialize source, dest = nil
     @source = source
     @tmp_dir = Dir.mktmpdir
     @dest_dir = dest || @tmp_dir
+
+    detect_type source
   end
 
   def files glob = '**/*'
@@ -14,7 +16,24 @@ class Sourcerer
 
 private
 
-    def detect_type source = @source
+    def detect_type source
+      @type = case source
+      # local/remote git repo
+      when /.git$/
+        :git
+
+      # github shorthand
+      when /^[A-Za-z0-9-]+\/[A-Za-z0-9\-_.]+$/
+        :git
+
+      # local/remote zip file
+      when /.zip$/
+        :zip
+
+      # local dir
+      else
+        :dir if Dir.exists?(source)
+      end
     end
 
     def copy_to_tmp
--- spec/lib/sourcerer_spec.rb
+++ spec/lib/sourcerer_spec.rb
@@ -3,37 +3,47 @@ describe Sourcerer do
   # TEST FOR ALL TYPES HERE
   #
   describe '#detect_type' do
-    before do
-      @sourcerer = Sourcerer.new 'foo'
-    end
-
     context 'with a dir' do
       it 'should detect local relative paths' do
-        expect(@sourcerer.send(:detect_type, 'spec/fixtures/source_dir')).to eq :dir
+        @sourcerer = Sourcerer.new 'spec/fixtures/source_dir'
+        # expect(@sourcerer.source).to eq File.expand_path('spec/fixtures/source_dir')
+        expect(@sourcerer.type).to eq :dir
       end
 
       it 'should detect local absolute paths' do
-        expect(@sourcerer.send(:detect_type, File.expand_path('spec/fixtures/source_dir'))).to eq :dir
+        @sourcerer = Sourcerer.new File.expand_path('spec/fixtures/source_dir')
+        expect(@sourcerer.type).to eq :dir
       end
     end
 
     context 'with a git repo' do
       it 'should detect local git repos' do
-        expect(@sourcerer.send(:detect_type, 'spec/fixtures/source.git')).to eq :git
+        @sourcerer = Sourcerer.new 'spec/fixtures/source.git'
+        expect(@sourcerer.type).to eq :git
       end
 
       it 'should detect remote git repos' do
-        expect(@sourcerer.send(:detect_type, 'https://github.com/brewster1134/sourcerer.git')).to eq :git
+        @sourcerer = Sourcerer.new 'https://github.com/brewster1134/sourcerer.git'
+        expect(@sourcerer.type).to eq :git
       end
 
       it 'should detect github shorthand repos' do
-        expect(@sourcerer.send(:detect_type, 'brewster1134/sourceror')).to eq :git
+        @sourcerer = Sourcerer.new 'brewster1134/sourceror'
+        # expect(@sourcerer.source).to eq 'https://github.com/brewster1134/sourcerer.git'
+        expect(@sourcerer.type).to eq :git
       end
     end
 
     context 'with a zip file' do
-      it 'should detect zip files' do
-        expect(@sourcerer.send(:detect_type, 'spec/fixtures/source.zip')).to eq :zip
+      it 'should detect local zip files' do
+        @sourcerer = Sourcerer.new 'spec/fixtures/source.zip'
+        # expect(@sourcerer.source).to eq File.expand_path('spec/fixtures/source.zip')
+        expect(@sourcerer.type).to eq :zip
+      end
+
+      it 'should detect remote zip files' do
+        @sourcerer = Sourcerer.new 'https://github.com/brewster1134/sourcerer/archive/master.zip'
+        expect(@sourcerer.type).to eq :zip
       end
     end
   end
@@ -52,31 +62,14 @@ describe Sourcerer do
     end
   end
 
-  describe '#detect_type' do
-    it 'should detect directories' do
-      @sourcerer = Sourcerer.new 'foo_dir'
-      expect(@sourcerer.send(:detect_type)).to eq :dir
-    end
-
-    it 'should detect zip files' do
-      @sourcerer = Sourcerer.new 'foo.zip'
-      expect(@sourcerer.send(:detect_type)).to eq :zip
-    end
-
-    it 'should detect git repos' do
-      @sourcerer = Sourcerer.new 'foo.git'
-      expect(@sourcerer.send(:detect_type)).to eq :git
-    end
-  end
-
   # Test common behavior for all supported source types
   #
   # All sources must have the following structure
   #
-  # |_ foo
-  # | |_ file.foo
   # |_ bar
   # | |_ file.bar
+  # |_ foo
+  # | |_ file.foo
   # |_ .hidden_foo
   #
   @test_sources = [
@@ -93,11 +86,11 @@ describe Sourcerer do
 
       describe '#files' do
         it 'should list all files' do
-          expect(@sourcerer.files).to match_array [
+          expect(@sourcerer.files).to match_array([
             'foo/file.foo',
             'bar/file.bar',
             '.hidden_foo'
-          ]
+          ])
         end
 
         it 'should list globbed files' do
