74cd46058926105a17aafe975dd9e30dfa6a6fc9
Ryan Brewster <brewster1134@gmail.com>
2016-10-28 18:27:01 -0700

    0.0.7.pre

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
+###### 0.0.7.pre
+* fixed missing gem dependency
+* config updates
+
 ###### 0.0.6.pre
 * MVP
--- lib/sourcerer/metadata.rb
+++ lib/sourcerer/metadata.rb
@@ -1,4 +1,4 @@
 module Sourcerer
-  VERSION = '0.0.6.pre'.freeze
+  VERSION = '0.0.7.pre'.freeze
   SUMMARY = 'Consume local & remote sources with ease'.freeze
 end
